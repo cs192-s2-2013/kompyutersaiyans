6642938fe27c6f6088531c689b49b08a8931aa35
unknown <extremetrainer@yahoo.com>
2014-02-04 20:28:01 +0800

    added hit counter, page to check number of visits, reset hit counter

---
+++ uPortalSpringJDBC/src/com/uportal/controller/CounterController.java
@@ -0,0 +1,41 @@
+package com.uportal.controller;
+
+import java.security.Principal;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CounterController {
+
+	@RequestMapping("/counter")
+	public String updateCounter(
+			@CookieValue(value = "hitCounter", defaultValue = "0") Long hitCounter,
+			HttpServletResponse response, ModelMap model, Principal principal ) {
+		
+		if(principal != null){
+			 String name = principal.getName();
+				model.addAttribute("username", name);
+		 	}
+		
+		Cookie cookie = new Cookie("hitCounter", hitCounter.toString());
+		response.addCookie(cookie);
+		return "counter";
+	}
+	
+	@RequestMapping("/reset")
+	public String checkCounter(
+			@CookieValue(value = "hitCounter", defaultValue = "0") Long hitCounter,
+			HttpServletResponse response) {
+		hitCounter -= hitCounter;
+		Cookie cookie = new Cookie("hitCounter", hitCounter.toString());
+		response.addCookie(cookie);
+		return "home";
+	}
+
+}
--- uPortalSpringJDBC/src/com/uportal/controller/HomePageController.java
+++ uPortalSpringJDBC/src/com/uportal/controller/HomePageController.java
@@ -1,14 +1,19 @@
 package com.uportal.controller;
 
 import java.security.Principal;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.ModelMap;  
+
 import com.uportal.services.UserService;
 
 @Controller
@@ -18,14 +23,17 @@ public class HomePageController {
  UserService userService;
 
  	
- @RequestMapping(value="/home", method = RequestMethod.GET)
-	public String homePage(ModelMap model, Principal principal ) {
+ @RequestMapping(value="/home", method = RequestMethod.GET )
+	public String homePage(ModelMap model, Principal principal,@CookieValue(value = "hitCounter", defaultValue = "0") Long hitCounter,
+			HttpServletResponse response ) {
 	 	if(principal != null){
 	 		String name = principal.getName();
 			model.addAttribute("username", name);
 			model.addAttribute("message", "Spring Security Custom Form example");
 	 	}
-		
+	 	hitCounter++;
+		Cookie cookie = new Cookie("hitCounter", hitCounter.toString());
+		response.addCookie(cookie);
 		return "home";
 
 	}
