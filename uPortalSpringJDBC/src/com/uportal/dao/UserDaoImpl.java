c6e7b1cc4d80ccc53ed1dc8d2eadfcc0088a0353
shefrancia <shefrancia@gmail.com>
2014-02-21 00:08:32 +0800

    User DAO

--- uPortalSpringJDBC/src/com/uportal/dao/UserDaoImpl.java
+++ uPortalSpringJDBC/src/com/uportal/dao/UserDaoImpl.java
@@ -21,36 +21,11 @@ public class UserDaoImpl implements UserDao {
 	DataSource dataSource;
 	
 	public int insertData(User user) {
-		
-		/*if(!isDBinit)//initialize db; create table; create user
-		{
-			try
-			{
-				System.out.println("DB init");
-				Connection cxn = DriverManager.getConnection("jdbc:mysql://localhost:3306/","root","");
-				Statement st = cxn.createStatement();
-				System.out.println("connection established");
-				st.execute("CREATE DATABASE IF NOT EXISTS userdb;");
-				st.execute("USE userdb;");
-				st.execute("CREATE table if not exists user (	    userid int not null auto_increment,    firstname varchar(255) DEFAULT null,    lastname varchar(255) DEFAULT null,    email varchar(255) DEFAULT null,    username varchar(255) DEFAULT null,    password varchar(255) DEFAULT null,    primary key (userid));");
-				st.execute("alter table userdb.user add column enabled tinyint(1) default true;");
-				st.execute("alter table userdb.user add column access varchar(50) default 'ROLE_USER';");
-				st.execute("CREATE user 'java'@'localhost' identified by 'eclipseisabitch';");
-				st.execute("grant all privileges on userdb to 'java'@'localhost' with grant option;");
-				st.execute("INSERT INTO `user` (`firstname`, `lastname`, `email`, `username`, `password`) VALUES ('Sherlyne', 'Francia', 'sherlyne@francia.com', 'sherlyne', 'francia'),	('Denise', 'Francisco', 'denise@francisco.com', 'denise', 'francisco'),	('Erwin', 'Sanchez', 'erwin@sanchez.com', 'erwin', 'sanchex'),	('Frank', 'Rayo', 'frank@rayo.com', 'frank', 'rayo'),	('Mark', 'Navata', 'mark@navata.com', 'mark', 'navata');");
-				cxn.close();
-				System.out.println("OK");
-			}
-			catch(Exception e)
-			{
-				e.printStackTrace();
-			}
-			isDBinit = true;
-		}*/
 		String department = user.getDepartment();
 		String dept = department.replaceAll("[^0-9]", "");
 		String sql = "INSERT INTO users "
 				+ "(firstname, lastname, email, username, password, collegeid, departmentid ) VALUES (?,?,?,?,?,?,?);";
+		
 
 		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 		final String usernameCheck = "select count(*) from users where username = ?;";
@@ -61,14 +36,32 @@ public class UserDaoImpl implements UserDao {
 		if (usernameResult == 0 && emailResult == 0){
 			ShaPasswordEncoder passwordEncoder = new ShaPasswordEncoder();
 			String hashedPassword = passwordEncoder.encodePassword(user.getPassword(),null);
-			jdbcTemplate.update(
-					sql,
-					new Object[] { user.getFirstName(), user.getLastName(),
-							user.getEmail(), user.getUsername(), hashedPassword, user.getCollege(), Integer.parseInt(dept) });
+			if(user.getCollege().toString().equals("null")){
+				
+				sql = "INSERT INTO users "
+						+ "(firstname, lastname, email, username, password ) VALUES (?,?,?,?,?);";
+				jdbcTemplate.update(
+						sql,
+						new Object[] { user.getFirstName(), user.getLastName(),
+								user.getEmail(), user.getUsername(), hashedPassword});
+			}else if(user.getDepartment().toString().equals("null")){
+				sql = "INSERT INTO users "
+						+ "(firstname, lastname, email, username, password, collegeid ) VALUES (?,?,?,?,?,?);";
+				jdbcTemplate.update(
+						sql,
+						new Object[] { user.getFirstName(), user.getLastName(),
+								user.getEmail(), user.getUsername(), hashedPassword, user.getCollege() });
+			}else{
+				jdbcTemplate.update(
+						sql,
+						new Object[] { user.getFirstName(), user.getLastName(),
+								user.getEmail(), user.getUsername(), hashedPassword, user.getCollege(), Integer.parseInt(dept) });
+			}
+			
 			sql = "select userid from users where username = ?";
 			int userid = (int)jdbcTemplate.queryForInt(
 					sql, new Object[] { user.getUsername() });
-			sql = "select typeid from userTypes where typename = 'GENERAL'";
+			sql = "select typeid from userTypes where typename = '" + user.getRole() + "'";
 			int typeid = (int)jdbcTemplate.queryForInt(sql);
 			sql = "INSERT INTO userRoles" 
 					+ "(userid, typeid) VALUES (?,?)";
